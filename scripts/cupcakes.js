d022ded9db55da164e28c0ebd20743b7eebdf147
BlazeSkyler <ucols@oregonstate.edu>
2024-05-14 11:53:55 -0700

    moved insert to top and fixed ui

--- scripts/cupcakes.js
+++ scripts/cupcakes.js
@@ -33,32 +33,32 @@ function domLoaded() {
 }
 
 function showform(selected) {
-    const view = document.getElementById("display");
     const insert = document.getElementById("insert");
+    const view = document.getElementById("display");
     const edit = document.getElementById("edit");
     const del = document.getElementById("delete");
 
     if (selected == "insert") {
-        view.style.display = "none";
         insert.style.display = "block";
+        view.style.display = "block";
         edit.style.display = "none";
         del.style.display = "none";
     }
     else if (selected == "edit") {
-        view.style.display = "none";
         insert.style.display = "none";
+        view.style.display = "none";
         edit.style.display = "block";
         del.style.display = "none";
     }
     else if (selected == "del") {
-        view.style.display = "none";
         insert.style.display = "none";
+        view.style.display = "none";
         edit.style.display = "none";
         del.style.display = "block";
     }
     else {
-        view.style.display = "block";
         insert.style.display = "none";
+        view.style.display = "block";
         edit.style.display = "none";
         del.style.display = "none";
     }
